4edea83bc898427a62b363878e9de85cb1d40804
UnluckyUse <josuelabhardt174@gmail.com>
2024-05-03 09:47:26 -0300

    Definida tela de cadastro como tela principal

---
+++ LeiloesTDSat/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Cria uma instância da tela de cadastro
+        cadastroVIEW telaCadastro = new cadastroVIEW();
+        // Configura a tela de cadastro como visível
+        telaCadastro.setVisible(true);
+    }
+}
+
+
